a47d27f06dd9a3e994e0ba43a13029d9939fe614
Daniel Sanabria <sanabriabrasil@hotmail.com>
2023-06-02 17:54:26 -0300

    First Comit

---
+++ RazorMVC/Pages/Index.cshtml.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RazorMVC.Pages;
+
+public class IndexModel : PageModel
+{
+    private readonly ILogger<IndexModel> _logger;
+
+    public IndexModel(ILogger<IndexModel> logger)
+    {
+        _logger = logger;
+    }
+
+    public string myFirstTextLiteral;
+
+    public void OnGet()
+    {
+        this.myFirstTextLiteral = "Hello, WORLD!";
+    }
+}
+
